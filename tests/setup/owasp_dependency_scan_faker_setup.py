ee91e3a4f1f330e431ba1037384493abd0967b8d
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2025-02-13 15:10:21 +0100

    feat: review-comment: tighten up OwaspFaker

--- tests/setup/owasp_dependency_scan_faker_setup.py
+++ tests/setup/owasp_dependency_scan_faker_setup.py
@@ -1,10 +1,9 @@
 # SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
 #
 # SPDX-License-Identifier: Apache-2.0
-from collections.abc import Sequence
-from typing import Any, cast
+from typing import cast
 
-from faker import Faker, Generator
+from faker import Faker
 
 from tests.input_formats.owasp_deependency_scan.entities.generators.owasp_dependency_report_model_provider import (  # noqa: E501
     OWASPDependencyReportModelProvider,
@@ -14,34 +13,23 @@ from tests.input_formats.owasp_deependency_scan.entities.generators.owasp_depend
 class OwaspFaker(Faker):
     owasp_provider: OWASPDependencyReportModelProvider
 
-    def __init__(
-        self,
-        locale: str | Sequence[str] | dict[str, int | float] | None = None,
-        providers: list[str] | None = None,
-        generator: Generator | None = None,
-        includes: list[str] | None = None,
-        use_weighting: bool = True,
-        **config: Any,
-    ):
-        super().__init__(
-            locale, providers, generator, includes, use_weighting, **config
-        )
+    def __init__(self) -> None:
+        owasp_provider = OWASPDependencyReportModelProvider(self)
         self.owasp_dependency_report_model = (
-            self.owasp_provider.owasp_dependency_report_model
+            owasp_provider.owasp_dependency_report_model
         )
-        self.project_info_model = self.owasp_provider.project_info_model
-        self.data_source_model = self.owasp_provider.data_source_model
-        self.scan_info_model = self.owasp_provider.scan_info_model
-        self.dependencies = self.owasp_provider.dependencies
-        self.dependency_model = self.owasp_provider.dependency_model
-        self.evidence_collected_model = self.owasp_provider.evidence_collected_model
-        self.evidence_model = self.owasp_provider.evidence_model
-        self.package_model = self.owasp_provider.package_model
-        self.package_models = self.owasp_provider.package_models
-        self.vulnerability_model = self.owasp_provider.vulnerability_model
+        self.project_info_model = owasp_provider.project_info_model
+        self.data_source_model = owasp_provider.data_source_model
+        self.scan_info_model = owasp_provider.scan_info_model
+        self.dependencies = owasp_provider.dependencies
+        self.dependency_model = owasp_provider.dependency_model
+        self.evidence_collected_model = owasp_provider.evidence_collected_model
+        self.evidence_model = owasp_provider.evidence_model
+        self.package_model = owasp_provider.package_model
+        self.package_models = owasp_provider.package_models
+        self.vulnerability_model = owasp_provider.vulnerability_model
 
 
 def setup_owasp_faker(faker: Faker) -> OwaspFaker:
     faker.add_provider(OWASPDependencyReportModelProvider)
-    faker = cast(OwaspFaker, faker)
-    return faker
+    return cast(OwaspFaker, faker)
