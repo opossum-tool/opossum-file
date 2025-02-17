0d3eaa5fcb5b19206f6287796c032da5a52f4c39
Adrian Braemer <adrian.braemer@tngtech.com>
2025-02-17 14:30:28 +0100

    test: move lonely test from test_get_attribution_info to the others

--- tests/input_formats/scancode/services/test_convert_to_opossum.py
+++ tests/input_formats/scancode/services/test_convert_to_opossum.py
@@ -7,11 +7,16 @@ import pytest
 
 from opossum_lib.core.entities.resource import ResourceType
 from opossum_lib.input_formats.scancode.constants import (
+    SCANCODE_SOURCE_NAME,
     SCANCODE_SOURCE_NAME_DEPENDENCY,
     SCANCODE_SOURCE_NAME_PACKAGE,
 )
 from opossum_lib.input_formats.scancode.services.convert_to_opossum import (
     convert_to_opossum,
+)
+from opossum_lib.shared.entities.opossum_input_file_model import (
+    OpossumPackageModel,
+    SourceInfoModel,
 )
 from tests.setup.scancode_faker_setup import ScanCodeFaker
 
@@ -320,6 +325,77 @@ class TestScancodeIndividualOptions:
         assert dep2.package_version == "42"
         assert dep2.package_namespace == "test"
         assert dep2.package_type == "dummy"
+
+    def test_license_conversion_produces_expected_result(
+        self, scancode_faker: ScanCodeFaker
+    ) -> None:
+        options = scancode_faker.options(package=False, license=True)
+        match1 = scancode_faker.match(
+            license_expression_spdx="Apache-2.0",
+            from_file="A",
+            score=75,
+            rule_relevance=50,
+        )
+        match2 = scancode_faker.match(
+            license_expression_spdx="Apache-2.0",
+            from_file="A",
+            score=95,
+            rule_relevance=50,
+        )
+        match3 = scancode_faker.match(
+            license_expression_spdx="MIT",
+            from_file="A",
+            score=50,
+            rule_relevance=50,
+        )
+        license1 = scancode_faker.license_detection(
+            license_expression_spdx="Apache-2.0",
+            matches=[match1, match2],
+        )
+        license2 = scancode_faker.license_detection(
+            license_expression_spdx="MIT",
+            matches=[match3],
+        )
+        copyright1 = scancode_faker.copyright(copyright="Me")
+        copyright2 = scancode_faker.copyright(copyright="Myself")
+        copyright3 = scancode_faker.copyright(copyright="I")
+        file = scancode_faker.single_file(
+            path="A",
+            license_detections=[license1, license2],
+            copyrights=[copyright1, copyright2, copyright3],
+            options=options,
+        )
+        scancode_data = scancode_faker.scancode_data(files=[file], options=options)
+        opossum = convert_to_opossum(scancode_data)
+        attributions = (
+            opossum.to_opossum_file_model().input_file.external_attributions.values()
+        )
+
+        expected1 = OpossumPackageModel(
+            source=SourceInfoModel(name=SCANCODE_SOURCE_NAME, document_confidence=50),
+            license_name="MIT",
+            copyright="Me\nMyself\nI",
+            attribution_confidence=50,
+        )
+        expected2 = OpossumPackageModel(
+            source=SourceInfoModel(name=SCANCODE_SOURCE_NAME, document_confidence=95),
+            license_name="Apache-2.0",
+            copyright="Me\nMyself\nI",
+            attribution_confidence=95,
+        )
+        assert len(attributions) == 2
+        attribution1, attribution2 = sorted(
+            attributions, key=lambda attr: attr.attribution_confidence or 0
+        )
+        assert attribution1.source == expected1.source
+        assert attribution1.license_name == expected1.license_name
+        assert attribution1.copyright == expected1.copyright
+        assert attribution1.attribution_confidence == expected1.attribution_confidence
+
+        assert attribution2.source == expected2.source
+        assert attribution2.license_name == expected2.license_name
+        assert attribution2.copyright == expected2.copyright
+        assert attribution2.attribution_confidence == expected2.attribution_confidence
 
 
 class TestConvertToOpossumFull:
